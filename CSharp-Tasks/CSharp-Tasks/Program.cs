fc0a12a23aa4989129c8e0dfa72949f04d0ae7e9
NsreenAlDaraghmeh <115452541+NsreenAlDaraghmeh@users.noreply.github.com>
2022-11-20 17:27:50 +0300

    Add files via upload

---
+++ CSharp-Tasks/CSharp-Tasks/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//task1
+Console.WriteLine("input any string :");
+string text = Console.ReadLine();
+Console.WriteLine(text);
+//task2
+int n = 20;
+bool pass = true;
+double sal = 19.99;
+string second_name = "nsreen";
+const char Basel = 'A';
+Console.WriteLine(n);
+Console.WriteLine(pass);
+Console.WriteLine(sal);
+Console.WriteLine(second_name);
+Console.WriteLine(Basel);
+//task3
+string []cars = { "toyota", "mercedes", "ford", "Porsche" };
+Console.WriteLine(cars[0]);
+Console.WriteLine(cars[1]);
+Console.WriteLine(cars[2]);
+Console.WriteLine(cars[3]);
+//task4
+Console.WriteLine("Input your firstname:");
+string first_name= Console.ReadLine();
+Console.WriteLine("Input your lastname:");
+string last_name =Console.ReadLine();
+Console.WriteLine("Input your year of birth:");
+string Birth_date =Console.ReadLine();
+Console.WriteLine(first_name + " "+last_name+" "+Birth_date);
+//task5
+int[] array = { 1, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+Console.WriteLine("element_0 : " + array[0]);
+Console.WriteLine("element_1 : " + array[1]);
+Console.WriteLine("element_2 : " + array[2]);
+Console.WriteLine("element_3 : " + array[3]);
+Console.WriteLine("element_4 : " + array[4]);
+Console.WriteLine("element_5 : " + array[5]);
+Console.WriteLine("element_6 : " + array[6]);
+Console.WriteLine("element_7 : " + array[7]);
+Console.WriteLine("element_8 : " + array[8]);
+Console.WriteLine("element_9 : " + array[9]);
+//task6
+int[] array_2 = { 2,5,8 };
+Console.Write("The Sum Of Array_2 = ");
+Console.Write( array_2[0] + array_2[1] + array_2[2]);
+
+
+
