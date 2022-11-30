5656966ecedf6893e13542cbc52009623e13de2e
nsreen <nsreengmail.com>
2022-11-30 06:43:19 +0000

    update

--- oop_tasks/29.11.2022/29.11.2022/29.11.2022/Program.cs
+++ oop_tasks/29.11.2022/29.11.2022/29.11.2022/Program.cs
@@ -79,18 +79,5 @@ namespace _29._11._2022
     abstract class Car
     {
 
-        public abstract void StartEngine();
-        public abstract void StopEngine();
-        public abstract void OpenDoor();
-        public abstract void CloseDoor();
-        public abstract void Honk();
-        public abstract void IncreaseSpeed();
-        public abstract void DecreaseSpeed();
-        public abstract void Breaks();
-        public abstract void OpenWindow();
-        public abstract void CloseWindow();
-        public abstract void AdjustSeat();
-        public abstract void CleanGlass();
-        public abstract void ChangeGear();
     }
 }
